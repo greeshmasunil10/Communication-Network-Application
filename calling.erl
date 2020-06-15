e76bafedfbf8da3465f2a2a8e1b5eec06991eb00
greeshmasunil10 <greeshmasunil10@gmail.com>
2020-06-14 23:52:54 -0400

    refactor

--- calling.erl
+++ calling.erl
@@ -1,53 +1,42 @@
 -module(calling).
 -import(lists,[foreach/2,nth/2]).
--export([start/1]).
+-export([startnetwork/1]).
+-export([sendmessage/2]).
+-import(exchange,[masterstatus/1]).
 
--export([passingto/2]).
--import(exchange,[printingmessages/1]).
-
-
-passingto(XParaname,TimoutTime)->
- receive
-    
-        {requestmessage,REQMsgInfo}->
-		                 RecieverPID=nth(1,REQMsgInfo),
-						 RecieverName=nth(2,REQMsgInfo),
-						 Timout=nth(3,REQMsgInfo),
-						 {A1,A2,A3} = now(),
-                         random:seed(A1, A2, A3),
-						 TimeDis=A3,
-						 Tun=[RecieverName,XParaname,TimeDis,Timout],
-						 whereis(exchange)!{masterequest,Tun},
-						 Sleeptimer2=rand:uniform(100),
-						 timer:sleep(Sleeptimer2),
-						 Tun1=[XParaname,RecieverName,TimeDis,Timout],
-						 RecieverPID ! {replymessege,Tun1},
-						passingto(XParaname,Timout);
-         
-		 {replymessege,Rlist}->
-		                CN=nth(1,Rlist),
-						RecieverName=nth(2,Rlist),
-						Timout=nth(4,Rlist),
-						TimeDis1=nth(3,Rlist),
-						Yup=[CN,RecieverName,TimeDis1,Timout],
-		                whereis(exchange)!{mastereply,Yup},
-						passingto(XParaname,Timout)
-				after TimoutTime-> io:fwrite("Process ~p has received no calls for ~w seconds,ending... ~n",[XParaname,TimoutTime/1000])
-	end.
-
-	
-	
-
-start(Callslist)->
-  io:fwrite("Sending messages."),
+startnetwork(Callslist)->
   Sender=nth(1,Callslist),
   Recievers=nth(2,Callslist),
-  %R1list=[whereis(Sender),Recievers],
-  foreach(fun(C) -> REQMsgInfo=[whereis(C),C,length(Recievers)*2000],
+  foreach(fun(X) -> REQMsgInfo=[whereis(X),X,length(Recievers)*2000],
 					whereis(Sender) ! {requestmessage,REQMsgInfo}
 					end, Recievers),
-  %callingperson(Recievers,R1list),
   TimoutTime=4000,
-  passingto(Sender,TimoutTime).
+  sendmessage(Sender,TimoutTime).
+
+sendmessage(Sender,TimoutTime)->
+ receive
+    {requestmessage,REQMsgInfo}->
+		RecieverPID=nth(1,REQMsgInfo),
+		RecieverName=nth(2,REQMsgInfo),
+		Timout=nth(3,REQMsgInfo),
+		{MegaSecs, Secs, MicroSecs} = now(),
+        random:seed({MegaSecs, Secs, MicroSecs}),
+		whereis(masterID)!{masterequest,[RecieverName,Sender,MicroSecs,Timout]},
+		timer:sleep(rand:uniform(200)),
+		RecieverPID ! {replymessege,[Sender,RecieverName,MicroSecs,Timout]},
+		sendmessage(Sender,Timout); 
+	{replymessege,Rlist}->
+		RecieverName=nth(2,Rlist),
+		Timout=nth(4,Rlist),
+		TimeDis1=nth(3,Rlist),
+		Yup=[nth(1,Rlist),RecieverName,TimeDis1,Timout],
+		whereis(masterID)!{mastereply,Yup},
+		sendmessage(Sender,Timout)
+	after TimoutTime-> io:fwrite("Process ~p has received no calls for ~w seconds,ending... ~n",[Sender,TimoutTime/1000])
+ end.
+
+	
+	
+
 
 
--- exchange.erl
+++ exchange.erl
@@ -1,43 +1,29 @@
 -module(exchange).
--import(lists,[append/2]). 
--import(lists,[nth/2]).
 -export([start/0]).
--import(lists,[merge/1]).
--export([printingmessages/1]).
+-import(lists,[nth/2]).
+-export([masterstatus/1]).
 -import(lists, [map/2]).
 -export([spawn_processes/2]).
 
-
-printingmessages(Rt)->
+masterstatus(TimeoutTime)->
 	receive
 	    {masterequest,RequestInfo}->
-			Receiver1=nth(1,RequestInfo),
-			Sender1=nth(2,RequestInfo),
-			Timestamp1=nth(3,RequestInfo),
-			Ds=nth(4,RequestInfo),
-			io:fwrite("~w received intro message from ~w [~w]~n",[Receiver1,Sender1,Timestamp1]),
-			printingmessages(Ds);
-		  
+			io:fwrite("~w received intro message from ~w [~w]~n",[nth(1,RequestInfo),nth(2,RequestInfo),nth(3,RequestInfo)]),
+			masterstatus(nth(4,RequestInfo));
 		{mastereply,ReplyInfo}->
-		    Receiver2=nth(1,ReplyInfo),
-            Sender2=nth(2,ReplyInfo),
-            Timestamp2=nth(3,ReplyInfo),
-		    Ds=nth(4,ReplyInfo),
-		    io:fwrite("~w received reply message from ~w [~w]~n",[Receiver2,Sender2,Timestamp2]),
-		    printingmessages(Ds)
-	after Rt->io:fwrite("Master has received no replies for ~w seconds,ending...~n",[Rt/1000]),
-	unregister(exchange)
+		    io:fwrite("~w received reply message from ~w [~w]~n",[nth(1,ReplyInfo),nth(2,ReplyInfo),nth(3,ReplyInfo)]),
+		    masterstatus(nth(4,ReplyInfo))
+	after TimeoutTime->io:fwrite("Master has received no replies for ~w seconds,ending...~n",[TimeoutTime/1000])	
 end.
 	
 spawn_processes(Name,Calls) ->
   %io:fwrite("~n************ list: ~w end~n",[[Name,Calls]]),
-  ProcID = spawn(calling,start,[[Name,Calls]]),
+  ProcID = spawn(calling,startnetwork,[[Name,Calls]]),
   register(Name, ProcID),
-  ProcID
-  .
+  ProcID.
 
 start()->
-    register(exchange,self()),
+    register(masterID,self()),
 	X=file:consult("calls.txt"),
 	Lis=nth(2,tuple_to_list(X)),
 	io:fwrite("** Calls to be made **~n"),
@@ -47,7 +33,7 @@ start()->
 	  spawn_processes(Name,Calls)
     end , Lis),
 	io:fwrite("~n"),
-    printingmessages(3000).
+    masterstatus(3000).
 
 	
 
